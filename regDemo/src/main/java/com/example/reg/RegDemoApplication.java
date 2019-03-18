f51a9a74e2bc4a5fd759993865e562f75623f8e2
WoramethPhupanbai <w.phupanbai@gmail.com>
2019-03-18 16:21:48 +0700

    Demo

---
+++ regDemo/src/main/java/com/example/reg/RegDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegDemoApplication.class, args);
+	}
+
+}
