2306143b46e14fd708576ef7a596f1e2c98a5170
WoramethPhupanbai <w.phupanbai@gmail.com>
2019-03-18 16:36:00 +0700

    add Override SpringApplicationBuilder Method

--- regDemo/src/main/java/com/example/reg/RegDemoApplication.java
+++ regDemo/src/main/java/com/example/reg/RegDemoApplication.java
@@ -2,10 +2,19 @@ package com.example.reg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.expression.spel.ast.Projection;
+
+
 
 @SpringBootApplication
-public class RegDemoApplication {
+public class RegDemoApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Projection.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RegDemoApplication.class, args);
 	}
