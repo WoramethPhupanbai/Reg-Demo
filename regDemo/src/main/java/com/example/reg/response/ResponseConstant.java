1701504f74c074f226aafdd1a64ccdd1ad7e533f
WoramethPhupanbai <w.phupanbai@gmail.com>
2019-03-21 16:33:05 +0700

    -update Class Response

---
+++ regDemo/src/main/java/com/example/reg/response/ResponseConstant.java
@@ -0,0 +1,17 @@
+package com.example.reg.response;
+
+public class ResponseConstant {
+	public static String resCode = "resCode";
+	public static String resDesc = "resDesc";
+	public static String resDescHeader = "resDescHeader";
+	public static String resCode_Success = "0";
+	public static String resCode_Exception = "1";
+	public static String successMessage = "SUCCESS";
+	public static String updateSuccessMessage = "Update Success";
+	public static String insertSuccessMessage = "Insert Success";
+	public static String errorNullMessage = "ข้อมูลไม่ถูกต้องโปรดติดต่อผู้ดูแลระบบ";
+	public static String errorDupMessage = "Duplicate Name!!";
+	public static String errorDupCodeMessage = "Duplicate Code!!";
+	public static String errorRuleRecommendMessage = "ข้อมูลไม่ถูกต้องโปรดติดต่อผู้ดูแลระบบ [ไม่พบข้อมูลจาก rule]";
+
+}
